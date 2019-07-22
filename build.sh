d596eb8cdede742a87d882ecf7c56c84fd525c7f
ElectRocnic <awiesing90@gmail.com>
2019-07-22 10:55:20 +0200

    debug ci build problem...

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 
 
 . ./resolve_os.sh
-. ./backend/.venv/"${VBIN}"/activate
+pwd
+ls -la
+ls -la /backend
+ls -la /backend/.venv
+ls -la /backend/.venv/bin
+. /backend/.venv/"${VBIN}"/activate
 
 cd client
 
