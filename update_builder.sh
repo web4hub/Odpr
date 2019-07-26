22b272a97ae003daabcacd496c6d52735bbf928f
ElectRocnic <electrocnicofficial@gmail.com>
2019-07-26 17:46:30 +0200

    Ahh, yesh yesh, the ARG instruction is MANDATORY in the Dockerfile if you want to use/consume --build-arg arguments. Then you can override them with ENVs to persist them in the image if needed.

--- update_builder.sh
+++ update_builder.sh
@@ -22,8 +22,6 @@ compare_images() {
 	fi
 }
 
-echo "args in builder.sh: ${WORKING_DIRECTORY} ${CLIENT_DIRECTORY} ${BACKEND_DIRECTORY} ${VERSION_DIRECTORY}"
-
 set +e
 docker pull "${BUILDER_IMAGE}" 2>/dev/null
 if [ ! $? -eq 0 ]; then
