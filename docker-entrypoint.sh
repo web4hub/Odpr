86e1e55dd9f60d8a854f03c03f113a32e8290283
ElectRocnic <electrocnicofficial@gmail.com>
2019-07-29 00:18:00 +0200

    The app image does not need to use a .venv and has it not even installed. Currently, nginx ssl keys are not yet available, and fixed the staticfiles location in the nginx paths. The wrong docker-entrypoint.sh has been copied into the docker image of postgres due to the fact that the context is in the parent directory where another docker-entrypoint.sh exists.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -Eeo pipefail
 sleep 5
 
 cd "${APP_IMAGE_BACKEND_DIRECTORY}"
-. "${APP_IMAGE_VENV_DIRECTORY}/bin/activate"
 python3 manage.py migrate
 
 gunicorn --chdir "${APP_IMAGE_BACKEND_DIRECTORY}" --bind :${GUNICORN_PORT} vuedj.wsgi:application
