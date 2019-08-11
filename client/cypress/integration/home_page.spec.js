81a45f4742ca4613bcd756e17020bfd890712b47
ElectRocnic <electrocnicofficial@gmail.com>
2019-08-11 20:45:28 +0200

    Updated cypress test with additional query to make sure the website is also displayed correctly. Fixed a bug.

--- client/cypress/integration/home_page.spec.js
+++ client/cypress/integration/home_page.spec.js
@@ -4,5 +4,6 @@ describe('The Home Page', function() {
   it('successfully loads', function() {
     visit()
 		getApp().should('be.visible')
+		getApp().contains('Welcome to your Django - Vue.js app!')
   })
 })
