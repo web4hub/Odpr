086e85bb7a0fb66fc4c1c97be96ec4d591d75962
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-04 23:28:42 +0200

    More debugging

--- util/run_lighthouse.sh
+++ util/run_lighthouse.sh
@@ -15,13 +15,23 @@ webpack_version=$(cat "${CLIENT_DIRECTORY}/package.json" | sed -rn 's~^.*"webpac
 vue_version=$(cat "${CLIENT_DIRECTORY}/package.json" | sed -rn 's~^.*"vue":.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
 django_version=$(cat "${BACKEND_DIRECTORY}/requirements.txt" | sed -rn 's~^.*Django==.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
 
+echo "Creating badge for project version with version=\"${CI_BUILD_TAG}\""
 anybadge -l version -v ${CI_BUILD_TAG} -f latest_release_tag.svg -c '#00FF00'
+echo "Creating badge for nginx version with version=\"${nginx_version}\""
 anybadge -l nginx -v ${nginx_version} -f nginx_version.svg -c '#008080'
+echo "Creating badge for postgres version with version=\"${postgres_version}\""
 anybadge -l postgres -v ${postgres_version} -f postgres_version.svg -c '#008080'
+echo "Creating badge for webpack version with version=\"${webpack_version}\""
 anybadge -l webpack -v ${webpack_version} -f webpack_version.svg -c '#008080'
+echo "Creating badge for vue version with version=\"${vue_version}\""
 anybadge -l vue -v ${vue_version} -f vue_version.svg -c '#008080'
+echo "Creating badge for django version with version=\"${django_version}\""
 anybadge -l django -v ${django_version} -f django_version.svg -c '#008080'
+echo "Creating badge for lighthouse performance with value=\"${performance}\""
 anybadge -l "lighthouse performance" -v ${performance} -f performance.svg 0.4=red 0.6=orange 0.8=yellow 1.01=green
+echo "Creating badge for lighthouse accessibility with value=\"${accessibility}\""
 anybadge -l "lighthouse accessibility" -v ${accessibility} -f accessibility.svg 0.4=red 0.6=orange 0.8=yellow 1.01=green
+echo "Creating badge for lighthouse best practices with value=\"${best_practices}\""
 anybadge -l "lighthouse best practices" -v ${best_practices} -f best_practices.svg 0.4=red 0.6=orange 0.8=yellow 1.01=green
+echo "Creating badge for lighthouse search engine results ranking with value=\"${seo}\""
 anybadge -l "lighthouse search engine results ranking" -v ${seo} -f search_engine_results_ranking.svg 0.4=red 0.6=orange 0.8=yellow 1.01=green
