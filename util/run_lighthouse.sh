d0155256c0535f9352388de1bdf28e96692cbd74
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-05 14:24:42 +0200

    Debug new badges...

--- util/run_lighthouse.sh
+++ util/run_lighthouse.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 set -eo pipefail
 
+echo "Running lighthouse analysis..."
 lighthouse "https://${TEST_PRODUCTION_DOMAIN}" --quiet --chrome-flags="--headless --no-sandbox" --no-enable-error-reporting --output-path=./report.html
 
+echo "Parse lighthouse results from generated report.html..."
 performance=$(cat report.html | sed -rn 's~^.*("id":"performance","score":)([[:digit:]]\.?[[:digit:]]{0,2}).*$~\2~p')
 accessibility=$(cat report.html | sed -rn 's~^.*("id":"accessibility","score":)([[:digit:]]\.?[[:digit:]]{0,2}).*$~\2~p')
 best_practices=$(cat report.html | sed -rn 's~^.*("id":"best-practices","score":)([[:digit:]]\.?[[:digit:]]{0,2}).*$~\2~p')
 seo=$(cat report.html | sed -rn 's~^.*("id":"seo","score":)([[:digit:]]\.?[[:digit:]]{0,2}).*$~\2~p')
 
+echo "Get nginx version by starting nginx docker container..."
 nginx_version=$(docker run -t --entrypoint nginx "${NGINX_IMAGE_DEBUG}" -v | sed -rn 's~^.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
+echo "Get postgres version by starting postgres docker container..."
 postgres_version=$(docker run -t --entrypoint postgres "${POSTGRES_IMAGE_DEBUG}" -V | sed -rn 's~^.*\s([[:digit:]]+\.[[:digit:]]+\.?[[:digit:]]*).*$~\1~p')
 
+echo "Get webpack version through package.json"
 webpack_version=$(cat "${CLIENT_DIRECTORY}/package.json" | sed -rn 's~^.*"webpack":.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
+echo "Get vue version through package.json"
 vue_version=$(cat "${CLIENT_DIRECTORY}/package.json" | sed -rn 's~^.*"vue":.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
+echo "Get django version through requirements.txt"
 django_version=$(cat "${BACKEND_DIRECTORY}/requirements.txt" | sed -rn 's~^.*Django==.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
 
+echo "Get outdated pip dependencies through running app docker container..."
 outdated_pip_dependencies=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o --format freeze | wc -l)
+echo "Get list of outdated pip dependencies in pretty format..."
 outdated_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o)
+echo "Get list of all pip dependencies..."
 all_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list)
 
 dir=$(pwd)
+echo "${dir}"
 cd "${CLIENT_DIRECTORY}"
+echo "cd to $(pwd)"
+echo "Get outdated npm package count through npm outdated in client directory..."
 outdated_npm_packages=$(($(npm outdated | wc -l)-1))
+echo "Get list of outdated npm packages in pretty format..."
 outdated_npm_packages_list=$(npm outdated)
 cd "${dir}"
+echo "back in $(pwd)"
 
+echo "Generate outdated report html..."
 echo '<!doctype html>' > outdated.html
 echo '<html lang="en">' >> outdated.html
 echo '<head>' >> outdated.html
@@ -53,7 +69,7 @@ echo '</p>' >> outdated.html
 echo '</div>' >> outdated.html
 echo '</body>' >> outdated.html
 
-
+echo "Prepare results for badge creation..."
 if [ $performance -eq 1 ] || [ $performance -eq 0 ]; then
 	performance="${performance}.0"
 fi
@@ -106,3 +122,6 @@ echo "Creating badge for outdated pip dependencies with value=\"${outdated_pip_d
 anybadge -l "pip dependencies" -v "${outdated_pip_dependencies}" -f outdated_pip.svg 1=green 3=yellow 8=orange 1000=red
 echo "Creating badge for outdated npm dependencies with value=\"${outdated_npm_packages}\""
 anybadge -l "npm dependencies" -v "${outdated_npm_packages}" -f outdated_npm.svg 1=green 5=yellow 10=orange 1000=red
+
+echo "in dir: $(pwd)"
+ls -la
