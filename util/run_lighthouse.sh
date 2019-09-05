f460d366d0d961b34d5573cd3014dc305592daa5
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-05 15:37:59 +0200

    Add another job which can be scheduled to update the dependency badges constantly. Removed debugging logs.

--- util/run_lighthouse.sh
+++ util/run_lighthouse.sh
@@ -22,60 +22,6 @@ vue_version=$(cat "${CLIENT_DIRECTORY}/package.json" | sed -rn 's~^.*"vue":.*([[
 echo "Get django version through requirements.txt"
 django_version=$(cat "${BACKEND_DIRECTORY}/requirements.txt" | sed -rn 's~^.*Django==.*([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*$~\1~p')
 
-echo "Get outdated pip dependencies through running app docker container..."
-outdated_pip_dependencies=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o --format freeze | wc -l)
-echo "Get list of outdated pip dependencies in pretty format..."
-outdated_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o)
-echo "Get list of all pip dependencies..."
-all_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list)
-
-dir=$(pwd)
-echo "${dir}"
-cd "${CLIENT_DIRECTORY}"
-echo "cd to $(pwd)"
-ls -la
-echo "npm -v"
-npm -v
-echo "npm outdated"
-echo "Get outdated npm package count through npm outdated in client directory..."
-set +e # npm outdated returns 1 if found outdated packages
-npm outdated
-outdated_npm_packages=$(($(npm outdated | wc -l)-1))
-echo "Get list of outdated npm packages in pretty format..."
-outdated_npm_packages_list=$(npm outdated)
-set -e
-cd "${dir}"
-echo "back in $(pwd)"
-
-echo "Generate outdated report html..."
-echo '<!doctype html>' > outdated.html
-echo '<html lang="en">' >> outdated.html
-echo '<head>' >> outdated.html
-echo '<meta charset="utf-8">' >> outdated.html
-echo '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">' >> outdated.html
-echo '<title>Outdated Packages</title>' >> outdated.html
-echo '</head>' >> outdated.html
-echo '<body>' >> outdated.html
-echo '<div>' >> outdated.html
-echo '<h1>Outdated NPM Packages</h1>' >> outdated.html
-echo '<p>' >> outdated.html
-echo "${outdated_npm_packages_list}" >> outdated.html
-echo '</p>' >> outdated.html
-echo '</div>' >> outdated.html
-echo '<div>' >> outdated.html
-echo '<h1>Outdated pip dependencies</h1>' >> outdated.html
-echo '<p>' >> outdated.html
-echo "${outdated_pip_dependencies_list}" >> outdated.html
-echo '</p>' >> outdated.html
-echo '</div>' >> outdated.html
-echo '<div>' >> outdated.html
-echo '<h1>List of all pip dependencies</h1>' >> outdated.html
-echo '<p>' >> outdated.html
-echo "${all_pip_dependencies_list}" >> outdated.html
-echo '</p>' >> outdated.html
-echo '</div>' >> outdated.html
-echo '</body>' >> outdated.html
-
 echo "Prepare results for badge creation..."
 if [ $performance -eq 1 ] || [ $performance -eq 0 ]; then
 	performance="${performance}.0"
@@ -91,18 +37,6 @@ fi
 
 if [ $seo -eq 1 ] || [ $seo -eq 0 ]; then
 	seo="${seo}.0"
-fi
-
-if [ $outdated_pip_dependencies -le 0 ]; then
-	outdated_pip_dependencies="up to date"
-else
-	outdated_pip_dependencies="${outdated_pip_dependencies} newer available"
-fi
-
-if [ $outdated_npm_packages -le 0 ]; then
-	outdated_npm_packages="up to date"
-else
-	outdated_npm_packages="${outdated_npm_packages} newer available"
 fi
 
 echo "Creating badge for project version with version=\"${CI_BUILD_TAG}\""
@@ -125,10 +59,5 @@ echo "Creating badge for lighthouse best practices with value=\"${best_practices
 anybadge -l "lighthouse best practices" -v ${best_practices} -f best_practices.svg 0.55=red 0.8=orange 0.9=yellow 1.01=green
 echo "Creating badge for lighthouse search engine results ranking with value=\"${seo}\""
 anybadge -l "lighthouse search engine results ranking" -v ${seo} -f search_engine_results_ranking.svg 0.55=red 0.8=orange 0.9=yellow 1.01=green
-echo "Creating badge for outdated pip dependencies with value=\"${outdated_pip_dependencies}\""
-anybadge -l "pip dependencies" -v "${outdated_pip_dependencies}" -f outdated_pip.svg 1=green 3=yellow 8=orange 1000=red
-echo "Creating badge for outdated npm dependencies with value=\"${outdated_npm_packages}\""
-anybadge -l "npm dependencies" -v "${outdated_npm_packages}" -f outdated_npm.svg 1=green 5=yellow 10=orange 1000=red
 
-echo "in dir: $(pwd)"
-ls -la
+util/update_dependency_badges.sh
---
+++ util/update_dependency_badges.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -eo pipefail
+
+echo "Get outdated pip dependencies through running app docker container..."
+outdated_pip_dependencies=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o --format freeze | wc -l)
+echo "Get list of outdated pip dependencies in pretty format..."
+outdated_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list -o)
+echo "Get list of all pip dependencies..."
+all_pip_dependencies_list=$(docker run -t --entrypoint pip "${APP_IMAGE_DEBUG}" list)
+
+dir=$(pwd)
+cd "${CLIENT_DIRECTORY}"
+echo "Get outdated npm package count through npm outdated in client directory..."
+set +e # npm outdated returns 1 if found outdated packages
+outdated_npm_packages=$(($(npm outdated | wc -l)-1))
+echo "Get list of outdated npm packages in pretty format..."
+outdated_npm_packages_list=$(npm outdated)
+set -e
+cd "${dir}"
+
+echo "Generate outdated report html..."
+echo '<!doctype html>' > outdated.html
+echo '<html lang="en">' >> outdated.html
+echo '<head>' >> outdated.html
+echo '<meta charset="utf-8">' >> outdated.html
+echo '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">' >> outdated.html
+echo '<title>Outdated Packages</title>' >> outdated.html
+echo '</head>' >> outdated.html
+echo '<body>' >> outdated.html
+echo '<div>' >> outdated.html
+echo '<h1>Outdated NPM Packages</h1>' >> outdated.html
+echo '<p>' >> outdated.html
+echo "${outdated_npm_packages_list}" >> outdated.html
+echo '</p>' >> outdated.html
+echo '</div>' >> outdated.html
+echo '<div>' >> outdated.html
+echo '<h1>Outdated pip dependencies</h1>' >> outdated.html
+echo '<p>' >> outdated.html
+echo "${outdated_pip_dependencies_list}" >> outdated.html
+echo '</p>' >> outdated.html
+echo '</div>' >> outdated.html
+echo '<div>' >> outdated.html
+echo '<h1>List of all pip dependencies</h1>' >> outdated.html
+echo '<p>' >> outdated.html
+echo "${all_pip_dependencies_list}" >> outdated.html
+echo '</p>' >> outdated.html
+echo '</div>' >> outdated.html
+echo '</body>' >> outdated.html
+
+if [ $outdated_pip_dependencies -le 0 ]; then
+	pip_color="green"
+	outdated_pip_dependencies="up to date"
+else
+	if [ $outdated_pip_dependencies -le 2 ]; then
+		pip_color="yellow"
+	elif [ $outdated_pip_dependencies -le 7 ]; then
+		pip_color="orange"
+	else
+		pip_color="red"
+	fi
+	outdated_pip_dependencies="${outdated_pip_dependencies} newer available"
+fi
+
+if [ $outdated_npm_packages -le 0 ]; then
+	npm_color="green"
+	outdated_npm_packages="up to date"
+else
+	if [ $outdated_npm_packages -le 4 ]; then
+		npm_color="yellow"
+	elif [ $outdated_npm_packages -le 9 ]; then
+		npm_color="orange"
+	else
+		npm_color="red"
+	fi
+	outdated_npm_packages="${outdated_npm_packages} newer available"
+fi
+
+echo "Creating badge for outdated pip dependencies with value=\"${outdated_pip_dependencies}\""
+anybadge -l "pip dependencies" -v "${outdated_pip_dependencies}" -f outdated_pip.svg -c "${pip_color}"
+echo "Creating badge for outdated npm dependencies with value=\"${outdated_npm_packages}\""
+anybadge -l "npm dependencies" -v "${outdated_npm_packages}" -f outdated_npm.svg -c "${npm_color}"
+
