42967bb6107826e09c497cca60ca205eae6d7f6a
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-06 12:53:55 +0200

    Possible fix for a bug in the new credentials script.

--- util/prepare_ssh_credentials.sh
+++ util/prepare_ssh_credentials.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+echo "apt-get update -qq"
 apt-get update -qq
-'which ssh-agent || ( apt-get install -qq openssh-client )'
+echo "which ssh-agent || ( apt-get install -qq openssh-client )"
+which ssh-agent || ( apt-get install -qq openssh-client )
+echo "eval (ssh-agent -s)"
 eval $(ssh-agent -s)
+echo "ssh-add <(cat \"DEPLOY_SERVER_PRIVATE_KEY\")"
 ssh-add <(cat "$DEPLOY_SERVER_PRIVATE_KEY")
+echo "mkdir -p ~/.ssh"
 mkdir -p ~/.ssh
-'[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
+echo "[[ -f /.dockerenv ]] && echo -e \"Host *\\n\\tStrictHostKeyChecking no\\n\\n\" > ~/.ssh/config"
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
