2588f8dae1ea28779397201f3e90e1e19db040be
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-06 14:30:17 +0200

    Ahh, ssh-add is only active for the current bash session, which is not the same when I run it within a script and then outside in the gitlab-ci.yml or between script and after_script etc... thus it did not work anymore. makes sense.

--- util/prepare_ssh_credentials.sh
+++ util/prepare_ssh_credentials.sh
@@ -7,8 +7,6 @@ echo "which ssh-agent || ( apt-get install -qq openssh-client )"
 which ssh-agent || ( apt-get install -qq openssh-client )
 echo "eval (ssh-agent -s)"
 eval $(ssh-agent -s)
-echo "ssh-add <(cat \"DEPLOY_SERVER_PRIVATE_KEY\")"
-ssh-add <(cat "$DEPLOY_SERVER_PRIVATE_KEY")
 echo "mkdir -p ~/.ssh"
 mkdir -p ~/.ssh
 echo "[[ -f /.dockerenv ]] && echo -e \"Host *\\n\\tStrictHostKeyChecking no\\n\\n\" > ~/.ssh/config"
