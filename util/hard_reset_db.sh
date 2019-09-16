bca947a75f6eeeb1a4890c3440128fb5d0d0fe85
ElectRocnic <electrocnicofficial@gmail.com>
2019-09-16 21:48:42 +0200

    Merge latest boilerplate update into app and fix migration setup.

--- util/hard_reset_db.sh
+++ util/hard_reset_db.sh
@@ -12,13 +12,20 @@ rm -rf ../backend/accounts/migrations
 echo ".../accounts/migrations"
 rm -rf ../backend/api/migrations
 echo ".../api/migrations"
-rm -rf ../backend/app/migrations
-echo ".../app/migrations"
+rm -rf ../backend/odpr_shared_models/migrations
+echo ".../odpr_shared_models/migrations"
+rm -rf ../backend/sceneries/migrations
+echo ".../sceneries/migrations"
+rm -rf ../backend/scenery_requests/migrations
+echo ".../scenery_requests/migrations"
+rm -rf ../backend/documentation/migrations
+echo ".../documentation/migrations"
 
 echo "Making migrations..."
-python ../backend/manage.py makemigrations accounts api app
+#python ../backend/manage.py makemigrations accounts odpr_shared_models documentation sceneries scenery_requests
+#python ../backend/manage.py makemigrations
 
 echo "Migrating..."
-python ../backend/manage.py migrate
+#python ../backend/manage.py migrate
 
 echo "Finished. You can now create a new superuser by registering a user at localhost:8000/api/v1/auth/register/ (don't forget the trailing slash/)"
