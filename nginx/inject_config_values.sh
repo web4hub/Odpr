fd3f7fba51c344ec0d9cc6d136fdf8fe9692857b
ElectRocnic <awiesing90@gmail.com>
2019-09-27 11:01:53 +0200

    Possible fix for www to non-www still not working: directly redirect both, port 80 and port 443 requests to non-www port 443.

--- nginx/inject_config_values.sh
+++ nginx/inject_config_values.sh
@@ -13,7 +13,5 @@ set -eo pipefail
 /inject_env_into_file.sh NGINX_IMAGE_MEDIAFILES_DESTINATION_DIRECTORY /etc/nginx/sites-customizations/local.443.conf --force
 
 /inject_env_into_file.sh NGINX_SERVER_NAME /etc/nginx/sites-customizations/www.80.conf --force
-/inject_env_into_file.sh NGINX_PERMANENT_REDIRECT_INSTRUCTION /etc/nginx/sites-customizations/www.80.conf --force
 
 /inject_env_into_file.sh NGINX_SERVER_NAME /etc/nginx/sites-customizations/www.443.conf --force
-/inject_env_into_file.sh NGINX_PERMANENT_REDIRECT_INSTRUCTION /etc/nginx/sites-customizations/www.443.conf --force
