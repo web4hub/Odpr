2d9518e097e5ecda55694a91791d4f29287178f9
ElectRocnic <electrocnicofficial@gmail.com>
2019-10-05 16:48:57 +0200

    Inject VUE_DEBUG variable also to store, as there, the devtools setting was set to false hardcoded. Of course it would be nicer to re-use the injected variable from another file, but also more complicated.

--- client/src/_store/index.js
+++ client/src/_store/index.js
@@ -4,9 +4,20 @@ import Vuex from 'vuex'
 import alert from './alert.module'
 import account from './modules/sessionHandling/account.module'
 
-Vue.use(Vuex)
+/* This nasty hack is an ugly workaround to provide the following feature:
+* 1. If app is built locally without any docker or environment variable being injected below, _DEBUG
+* will be set to true so that Vue.config.productionTip and Vue.config.devtools will be resolved to true, because
+* it is most likely that somebody building the app locally will want to debug the app and not build a new production release.
+* 2. If app is built on gitlab ci, depending on the image version either true or false will be injected, so that the
+* production image does not activate debug settings.
+* */
+let _DEBUG = 'VUE_DEBUG' // the String VUE_DEBUG will be replaced by .gitlab-ci injections if needed. If not replaced, it will be replaced by an empty string below.
+if (_DEBUG.includes('VUE_DEBU') && _DEBUG.includes('UE_DEBUG')) { // hack to prevent the injection to overwrite the test string as well
+	_DEBUG = 'true'
+}
+Vue.config.devtools = (_DEBUG === 'true')
 
-Vue.config.devtools = false
+Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	// DONE: add strict: true // TODO: disable for production!
