8ca8cea7d68ea076c8c0a94d08f73dcf0c5f2230
ElectRocnic <electrocnicofficial@gmail.com>
2019-10-26 22:14:51 +0200

    Fix some eslint errors.

--- client/src/_util/getObjectByArray.js
+++ client/src/_util/getObjectByArray.js
@@ -1,6 +1,6 @@
 export default function getByKey (object, array) {
 	for (let i = 0, n = array.length; i < n; ++i) {
-		let k = array[i]
+		const k = array[i]
 		if (k in object) {
 			object = object[k]
 		} else {
--- client/src/_util/getObjectByKey.js
+++ client/src/_util/getObjectByKey.js
@@ -1,9 +1,9 @@
 export default function getByKey (object, key) {
 	key = key.replace(/\[(\w+)\]/g, '.$1') // convert indexes to properties
 	key = key.replace(/^\./, '') // strip a leading dot
-	let a = key.split(/[/.]/)
+	const a = key.split(/[/.]/)
 	for (let i = 0, n = a.length; i < n; ++i) {
-		let k = a[i]
+		const k = a[i]
 		if (k in object) {
 			object = object[k]
 		} else {
