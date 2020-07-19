a6f433b57fc0cb00539bb2eef859d4a8df55a7da
ElectRocnic <electrocnicofficial@gmail.com>
2020-07-19 14:54:34 +0200

    Add missing return/echo for debian-dind updater

--- util/update_builder_and_dind_image.sh
+++ util/update_builder_and_dind_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
-sh debian-dind/update_dind_image.sh
-if [ ! $? -eq 0 ]; then
+result=$(sh debian-dind/update_dind_image.sh)
+if [ ${result} == "UPDATED" ]; then
 	echo "Debian-Dind needed an update so we also update builder..."
 	export BUILD=1
 fi
