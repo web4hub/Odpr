7e381e3365e79a44e5378166080bfe96e2eadac1
ElectRocnic <electrocnicofficial@gmail.com>
2020-07-19 14:33:50 +0200

    Possible fix for "permission denied"

--- util/update_builder_and_dind_image.sh
+++ util/update_builder_and_dind_image.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "pwd:"
-pwd
-
-debian-dind/update_dind_image.sh
+sh debian-dind/update_dind_image.sh
 if [ ! $? -eq 0 ]; then
 	echo "Debian-Dind needed an update so we also update builder..."
 	export BUILD=1
 fi
 
-builder/update_builder.sh
+sh builder/update_builder.sh
